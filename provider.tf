9a3a7d976cb1b08fcacd69e474358d762e6cb6cc
fabi8483 <fabian.salamanca@rackspace.com>
2021-08-20 10:59:31 -0500

    Added resource delete lock

--- provider.tf
+++ provider.tf
@@ -17,4 +17,17 @@ terraform {
 resource "azurerm_resource_group" "azagentrg" {
   name     = var.resgroup
   location = var.milocation
+
+  tags {
+    appid-or-sso = "fabi8483"
+    Description = "PSBuild Pipeline"
+    Environment = "Sandbox"
+  }
+}
+
+resource "azurerm_management_lock" "azagentrg-lock" {
+  name       = "resource-group-level"
+  scope      = azurerm_resource_group.example.id
+  lock_level = "CanNotDelete"
+  notes      = "Protect this resource for RAX PS"
 }
